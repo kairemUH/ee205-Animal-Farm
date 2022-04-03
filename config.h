0e3553be6f04089ffdd57eafa0ce47d7fa731109
kairemUH <kairem@hawaii.edu>
2022-04-02 21:27:30 -1000

    Created config file

---
+++ config.h
@@ -0,0 +1,22 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  Animal Farm 2 - EE 205 - Spr 2022
+///
+/// @file config.h
+/// @version 1.0
+///
+/// @author Kai Matsusaka <kairem@hawaii.edu>
+/// @date   02_Apr_2022
+///////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#define MAX_CAT_NAME (50)
+
+typedef float Weight ;
+
+enum Gender {UNKNOWN_GENDER, MALE, FEMALE};
+
+enum Breed {UNKNOWN_BREED, MAINE_COON, MANX, SHORTHAIR, PERSIAN, SPHYNX};
+
+enum Color {BLACK, WHITE, RED, BLUE, GREEN, PINK};
