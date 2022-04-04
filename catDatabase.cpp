451fe080603063681c71d9dcba50f88b250af63f
kairemUH <kairem@hawaii.edu>
2022-04-03 16:18:15 -1000

    Cat Database refactored

--- catDatabase.cpp
+++ catDatabase.cpp
@@ -9,189 +9,45 @@
 /// @date   19_Mar_2022
 ///////////////////////////////////////////////////////////////////////////////
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <string.h>
+#include <iostream>
+#include <cassert>
 
+#include "Cat.h"
 #include "catDatabase.h"
 #include "addCats.h"
 #include "reportCats.h"
-#include "updateCats.h"
 #include "deleteCats.h"
 
-struct Cat catArray[MAX_CAT]; //Cat Database
-
-NumCats numCats = 0;              //The high water mark for the array (The number of cats in the array)
+Cat* catDatabaseHeadPointer = nullptr;
 
 
-////////////////////  Enum Functions  ////////////////////
+bool isCatInDatabase( const Cat* cat ) {
 
-const char* genderName( enum Gender gender ) {
-
-    switch (gender) {
-        case 0:
-            return "Unknown Gender";
-            break;
-        case 1:
-            return "Male";
-            break;
-        case 2:
-            return "Female";
-            break;
-        default:
-            return "Unknown Gender";
-            break;
-    }
-
-}
-
-
-const char* breedName( enum Breed breed ) {
-
-    switch (breed) {
-        case 0:
-            return "Unknown Breed";
-            break;
-        case 1:
-            return "Maine Coon";
-            break;
-        case 2:
-            return "Manx";
-            break;
-        case 3:
-            return "Shorthair";
-            break;
-        case 4:
-            return "Persian";
-            break;
-        case 5:
-            return "Sphynx";
-            break;
-        default:
-            return "Unknown Gender";
-            break;
-    }
-
-}
-
-
-const char* colorName( enum Color color ) {
-
-    switch (color) {
-        case 0:
-            return "Black";
-            break;
-        case 1:
-            return "White";
-            break;
-        case 2:
-            return "Red";
-            break;
-        case 3:
-            return "Blue";
-            break;
-        case 4:
-            return "Green";
-            break;
-        case 5:
-            return "Pink";
-            break;
-        default:
-            return "Unknown Color";
-            break;
-    }
-
-}
-
-
-////////////////////  Validation Functions  ////////////////////
-
-bool nameIsValid( const char name[] ) {
-
-    if( strlen(name) < 1 ) {
-
-        fprintf( stderr, "Cat name cannot be empty.\n" );
+    if( cat == nullptr || !validateDatabase() ) {
         return false;
-
     }
 
-    if ( strlen(name) > MAX_CAT_NAME ) {
-
-        fprintf( stderr, "Cat name [%s] must be shorter than or equal to [%d] characters.\n", name, MAX_CAT_NAME );
-        return false;
-
-    }
-
-    return true;
-
-}
-
-
-bool nameInDatabase( const char name[] ) {
-
-    for ( int i = 0; i < numCats; i++ ) {
-
-        if( strcmp(catArray[i].name, name) == 0 ) {
-
-            fprintf( stderr, "Cat name [%s] already in database.\n", name );
-            return true;
-
+    for( Cat* iCat = catDatabaseHeadPointer; iCat != nullptr; iCat = iCat->next ) {
+        if( iCat == cat ) {
+            return true ;
         }
-
     }
 
-    return false;
+    assert( validateDatabase() ) ;
+
+    return false ;
 
 }
 
 
-bool weightIsValid( Weight weight ) {
+bool validateDatabase() {
 
-    if ( weight <= 0 ) {
-
-        fprintf( stderr, "Cat weight [%f] must be greater than zero.\n", weight );
-        return false;
-
+    for( Cat* iCat = catDatabaseHeadPointer; iCat != nullptr; iCat = iCat->next ) {
+        if( ! iCat->catIsValid() ) {
+            return false;
+        }
     }
 
     return true;
 
 }
-
-
-bool isFull() {
-
-    if ( numCats >= MAX_CAT ) {
-
-        fprintf( stderr, "Database is Full.\n" );
-        return true;
-
-    }
-
-    return false;
-
-}
-
-
-bool indexIsValid( int index ) {
-
-    if( numCats == 0 ) {
-        fprintf( stderr, "Cat Database is empty.\n" );
-        return false;
-    }
-
-    if( index < 0 || index > numCats ) {
-        fprintf( stderr, "Animal Farm 1: Bad Cat! Index [%d] must be greater than zero.\n", index );
-        return false;
-    }
-
-    if( index < 0 || index > numCats ) {
-        fprintf( stderr, "Animal Farm 1: Bad Cat! Index [%d] must be less than [%d]\n", index, numCats );
-        return false;
-    }
-
-    return true;
-
-}
-
--- catDatabase.h
+++ catDatabase.h
@@ -11,55 +11,13 @@
 
 #pragma once
 
-#define MAX_CAT 1024
-#define MAX_CAT_NAME 50
+#include "Cat.h"
 
-typedef float Weight;
+extern Cat* catDatabaseHeadPointer;
+extern bool isCatInDatabase( const Cat* cat );
+extern bool validateDatabase();
 
-typedef int NumCats;
-
-
-
-enum Gender {UNKNOWN_GENDER, MALE, FEMALE};
-
-enum Breed {UNKNOWN_BREED, MAINE_COON, MANX, SHORTHAIR, PERSIAN, SPHYNX};
-
-enum Color {BLACK, WHITE, RED, BLUE, GREEN, PINK};
-
-struct Cat {
-
-    char name[MAX_CAT_NAME];
-    enum Gender gender;
-    enum Breed breed;
-    bool isFixed;
-    Weight weight;
-    enum Color collarColor1;
-    enum Color collarColor2;
-    unsigned long long license;
-
-};
-
-
-extern struct Cat catArray[MAX_CAT];
-
-extern NumCats numCats;
-
-
-extern const char* genderName( enum Gender gender );
-
-extern const char* breedName( enum Breed breed );
-
-extern const char* colorName( enum Color color );
-
-
-extern bool nameIsValid( const char name[] );
-
-extern bool nameInDatabase( const char name[] );
-
-extern bool weightIsValid( Weight weight );
-
-extern bool isFull();
-
-extern bool indexIsValid( int index );
-
+//@todo future implementation of duplicate name checking
 //@todo add initialize database funciton that memsets the arrays to zero
+
+
