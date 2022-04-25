4516edaf1191ae477ba258bd87101b9120d5d7ba
kairemUH <kairem@hawaii.edu>
2022-04-25 00:43:34 -1000

    Added List Class

---
+++ List.cpp
@@ -0,0 +1,83 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  animalFarm - EE 205 - Spr 2022
+///
+/// @file List.cpp
+/// @version 1.0
+///
+/// @author Kai Matsusaka <kairem@hawaii.edu>
+/// @date   24_Apr_2022
+///////////////////////////////////////////////////////////////////////////////
+
+#include <cassert>
+
+#include "List.h"
+#include "Node.h"
+
+////////////////////  Public Functions  ////////////////////
+
+bool List::empty() const noexcept {
+    return List::head == nullptr;
+}
+
+
+unsigned int List::size() const noexcept {
+    return List::count;
+}
+
+
+bool List::isIn( Node *aNode ) const {
+
+    for(Node* iNode = List::head ; iNode != nullptr ; iNode = iNode->next ) {
+        if( aNode == iNode ) {
+            return true;
+        }
+    }
+
+    return false;
+
+}
+
+
+bool List::isSorted() const noexcept {
+
+    Node* temp = head;
+
+    for( Node* iNode = List::head->next ; iNode != nullptr ; iNode = iNode->next ) {
+
+        if( temp > iNode ) { return false; }
+
+        temp = iNode;
+
+    }
+
+    return true;
+
+}
+
+
+Node * List::get_first() const noexcept {
+    return List::head;
+}
+
+
+void List::deleteAllNodes() noexcept {
+
+    assert( validate() );
+    while( head != nullptr ) {
+        pop_front();
+    }
+#ifdef DEBUG
+    cout << PROGRAM_NAME << ": All Nodes have been deleted" << endl ;
+#endif
+    assert( validate() );
+
+}
+
+
+
+////////////////////  Static Functions  ////////////////////
+
+Node * List::get_next( const Node *currentNode ) {
+    return currentNode->next;
+}
---
+++ List.h
@@ -0,0 +1,46 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  animalFarm - EE 205 - Spr 2022
+///
+/// @file List.h
+/// @version 1.0
+///
+/// @author Kai Matsusaka <kairem@hawaii.edu>
+/// @date   24_Apr_2022
+///////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include "Node.h"
+
+class List {
+
+////////////////////  Member Variables  ////////////////////
+protected:
+    Node* head = nullptr;
+    unsigned int count = 0;
+
+
+////////////////////  Public Functions  ////////////////////
+public:
+    bool empty() const noexcept;
+    unsigned int size() const noexcept;
+    bool isIn(Node *aNode) const;
+    bool isSorted() const noexcept;
+    Node * get_first() const noexcept;
+    void deleteAllNodes() noexcept;
+
+
+////////////////////  Abstract Functions  ////////////////////
+public:
+    virtual Node * pop_front() noexcept=0;
+    virtual void dump() const noexcept=0;
+    virtual bool validate() const noexcept=0;
+
+
+////////////////////  Static Functions  ////////////////////
+public:
+    static Node * get_next(const Node *currentNode);
+
+};
+
