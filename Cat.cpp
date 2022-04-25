e997f5316213792a657c7dd698697eac69cea6f9
kairemUH <kairem@hawaii.edu>
2022-04-24 23:17:09 -1000

    Refactored Cat Class

--- Cat.cpp
+++ Cat.cpp
@@ -18,6 +18,91 @@
 #include "Cat.h"
 #include "reportCats.h"
 
+const std::string Cat::SPECIES_NAME = "Felis catus";
+const Weight Cat::MAX_WEIGHT = Weight(40);   // 40 pounds
+
+
+////////////////////  Constructors  ////////////////////
+Cat::Cat( const std::string &newName ) : Mammal( MAX_WEIGHT, SPECIES_NAME ), name( newName ), isCatFixed( false ) {}
+
+Cat::Cat( const std::string &newName, const Color newColor, const bool newIsFixed, const Gender newGender, const Weight newWeight ) :
+Mammal( newColor, newGender, newWeight, MAX_WEIGHT, SPECIES_NAME ), name( newName ), isCatFixed( newIsFixed ) {}
+
+
+
+////////////////////  Getters and Setters  ////////////////////
+
+std::string Cat::getName() const noexcept {
+    return Cat::name;
+}
+
+
+void Cat::setName( const std::string &newName ) {
+    if( validateName(newName) ) {
+        Cat::name = newName;
+    }
+}
+
+
+bool Cat::isFixed() const noexcept {
+    return Cat::isCatFixed;
+}
+
+
+void Cat::fixCat() noexcept {
+    Cat::isCatFixed = true;
+}
+
+
+
+////////////////////  Dump and Speak Functions  ////////////////////
+
+std::string Cat::speak() const noexcept {
+
+    std::cout << "Meow" << std::endl;
+
+}
+
+
+void Cat::dump() const noexcept {
+
+    Mammal::dump();
+    FORMAT_LINE_FOR_DUMP( "Cat", "Name" ) << Cat::name << std::endl;
+    FORMAT_LINE_FOR_DUMP( "Cat", "Is Fixed" ) << Cat::isCatFixed << std::endl;
+
+}
+
+
+
+////////////////////  Validation Functions  ////////////////////
+
+bool Cat::validate() const noexcept {
+
+    if( Mammal::validate() && validateName(Cat::name) ) {
+        return true;
+    }
+
+    return false;
+
+}
+
+
+bool Cat::validateName( const std::string &newName ) {
+
+    if( newName.empty() ) {
+        throw std::length_error( "Cat name cannot be empty.");
+    }
+
+    if( newName.length() >= MAX_NAME ) {
+        throw std::length_error( "Name must be less than MAX_CAT_NAME." );
+    }
+
+    return true;
+}
+
+
+
+/*
 #define FORMAT_LINE( className, member ) std::cout << std::setw(8) << (className) << std::setw(20) << (member) << std::setw(52)
 
 void Cat::zeroOutMemberData() {
@@ -175,3 +260,4 @@ bool Cat::breedIsValid(enum Breed breed) {
 
     return true;
 }
+*/
--- Cat.h
+++ Cat.h
@@ -12,7 +12,48 @@
 #pragma once
 
 #include "config.h"
+#include "Mammal.h"
 
+class Cat : Mammal {
+
+////////////////////  Member Variables  ////////////////////
+public:
+    static const std::string SPECIES_NAME;
+    static const Weight MAX_WEIGHT;
+
+protected:
+    std::string name;
+    bool isCatFixed;
+
+
+////////////////////  Constructors  ////////////////////
+public:
+    Cat(const std::string &newName);
+    Cat(const std::string &newName, const Color newColor, const bool newIsFixed, const Gender newGender, const Weight newWeight);
+
+
+////////////////////  Getters and Setters  ////////////////////
+public:
+    std::string getName() const noexcept;
+    void setName(const std::string &newName);
+    bool isFixed() const noexcept;
+    void fixCat() noexcept;
+
+
+////////////////////  Dump and Speak Functions  ////////////////////
+public:
+    std::string speak() const noexcept override;
+    void dump() const noexcept override;
+
+
+////////////////////  Validation Functions  ////////////////////
+public:
+    bool validate() const noexcept override;
+    static bool validateName(const std::string &newName);
+
+};
+
+/*
 class Cat {
 
 ////////////////////  Member Variables  ////////////////////
@@ -76,4 +117,4 @@ public:
 private:
     void zeroOutMemberData();
 
-};
+}; */
