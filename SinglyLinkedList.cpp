f0b6445c084d829a397c0ff320df0436b37b3418
kairemUH <kairem@hawaii.edu>
2022-04-25 01:34:31 -1000

    Created Singly Linked List Class

---
+++ SinglyLinkedList.cpp
@@ -0,0 +1,90 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  animalFarm - EE 205 - Spr 2022
+///
+/// @file SinglyLinkedList.cpp
+/// @version 1.0
+///
+/// @author Kai Matsusaka <kairem@hawaii.edu>
+/// @date   25_Apr_2022
+///////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <cassert>
+
+#include "SinglyLinkedList.h"
+
+
+////////////////////  Functions  ////////////////////
+
+void  SinglyLinkedList::push_front( Node* newNode ) {
+
+    assert( newNode->validate() );
+
+    if( isIn(newNode) ) {
+        throw std::logic_error( "Node already in database." );
+    }
+
+    newNode->next = head;
+    head = newNode;
+
+    assert( SinglyLinkedList::validate() );
+
+}
+
+
+Node* SinglyLinkedList::pop_front() noexcept {
+
+    assert( head != nullptr );
+    assert( SinglyLinkedList::validate() );
+
+    Node*& iNode = head;
+    head = head->next;
+
+    assert( SinglyLinkedList::validate() );
+
+    return iNode;   // @todo should delete old head
+
+
+}
+
+
+void SinglyLinkedList::insert_after( Node* currentNode, Node* newNode ) {
+
+    assert( SinglyLinkedList::validate() );
+    assert( newNode->validate() );
+
+    if( isIn(newNode) ) {
+        throw std::logic_error( "Node already in database." );
+    }
+
+    if( ! isIn(currentNode) ) {
+        throw std::logic_error( "Current node is not in database." );
+    }
+
+    newNode->next = currentNode->next;
+    currentNode->next = newNode;
+
+}
+
+
+void SinglyLinkedList::dump() const noexcept {
+
+    for(Node* iNode = List::head ; iNode != nullptr ; iNode = iNode->next ) {
+        iNode->dump();
+    }
+
+}
+
+
+bool SinglyLinkedList::validate() const noexcept {
+
+    for(Node* iNode = List::head ; iNode != nullptr ; iNode = iNode->next ) {
+
+        if( ! iNode->validate() ) { return false; }
+
+    }
+
+    return true;
+
+}
---
+++ SinglyLinkedList.h
@@ -0,0 +1,32 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  animalFarm - EE 205 - Spr 2022
+///
+/// @file SinglyLinkedList.h
+/// @version 1.0
+///
+/// @author Kai Matsusaka <kairem@hawaii.edu>
+/// @date   25_Apr_2022
+///////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include "List.h"
+
+class SinglyLinkedList : public List {
+
+////////////////////  Constructors  ////////////////////
+public:
+    SinglyLinkedList() = default;
+
+
+////////////////////  Functions  ////////////////////
+public:
+    void  push_front( Node* newNode );  // Insert `newNode` to the beginning of the List
+    Node* pop_front() noexcept override; // Remove and return the first Node in the List
+    void insert_after( Node* currentNode, Node* newNode );  // Insert `newNode` after `currentNode`
+    void dump() const noexcept override;     // Output the contents of this container
+    bool validate() const noexcept override;
+
+};
+
